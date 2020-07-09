d975246881c62502ce5f118aba083199d7c18de8
Kamalesh Reddy Paluru <kamaleshpaluru07@gmail.com>
2020-07-09 16:21:22 -0400

    71A_0
    -wrong answer on test 13 for no reason?

---
+++ 71A/wtlw.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void){
+    int n;
+    scanf("%i", &n);
+    char word[n][100];
+    if(0 <= n && n <= 100){
+        for(int i = 0; i < n; i++){
+            scanf("%s", word[i]);
+        }
+    }
+    for(int k = 0; k < n; k++){
+        int j;
+        for(j = 0; word[k][j] != 0; ){
+            if(word[k][j] != 0){
+                j++;
+            }
+        }
+        if(j > 10){
+            printf("%c%i%c\n", word[k][0], j - 2, word[k][j - 1]);
+        }
+        else{
+            printf("%s\n", word[k]);
+        }
+    }
+}
