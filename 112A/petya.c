b2fa8246ebaf294851afac0e8be8ef01947e3ed3
Kamalesh Reddy Paluru <kamaleshpaluru07@gmail.com>
2020-07-18 22:09:42 -0400

    112A
    -finished petya yay

--- 112A/petya.c
+++ 112A/petya.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
-ans(char a, char b);
+void ans(char a, char b);
+
+int count = 0;
 
 int main(void){
     char words[2][100];
@@ -11,16 +14,25 @@ int main(void){
     for(int i = 0; i < 100; i++){
         lower_words[0][i] = tolower(words[0][i]);
         lower_words[1][i] = tolower(words[1][i]);
+    }    
+    if(strcmp(lower_words[0], lower_words[1]) == 0){
+        printf("%i\n", 0);
     }
-    //printf("%s, %s\n", lower_words[0], lower_words[1]);
-
-}
-
-ans(char a, char b){
-    if(a < b){
-        
-    }
-    else if(a > b){
-
+    int len = strlen(words[0]);
+    int count = 0;
+    for(int j = 0; j < len; j++){
+        if(lower_words[0][j] < lower_words[1][j]){
+            count--;
+        }
+        else if(lower_words[0][j] > lower_words[1][j]){
+            count++;
+        }
+        else{
+            count += 0;
+        }
+        if(count != 0){
+            printf("%i\n", count);
+            break;
+        }
     }
 }
