a682ff26e668e0b2eb9f4b811479f743a5ca3a54
Kamalesh Reddy Paluru <kamaleshpaluru07@gmail.com>
2020-07-23 12:07:57 -0400

    266A
    -removed each similar, consecutive stone; worked marvelously.

--- 266A/stone.c
+++ 266A/stone.c
@@ -3,12 +3,14 @@
 
 int main(void){
     int len;
-    char stone[len];
     scanf("%i\n", &len);
+    char stone[len];
     scanf("%s", stone);
     int count = 0;
+/*
+//YESTERDAY'S CODE
     for(int i = 0; i < len - 2; i++){
-        if(stone[i] == stone[i + 1] || stone[i + 1] == stone[i + 2]){
+        if((stone[i] == stone[i + 1]) || (stone[i + 1] == stone[i + 2])){
             count++; 
         }
     }
@@ -18,4 +20,18 @@ int main(void){
     else{
         printf("%i\n", count);
     }
+*/
+//TODAY'S CODE
+    for(int k = 0; k < len; k++){
+        for(int n = 0; n < len; n++){
+            if(stone[n] == stone[n + 1]){
+                for(int i = n; i < len; i++){
+                    stone[i] = stone[i + 1];
+                }
+            }
+        }
+    }
+//printf("\n");
+printf("%lu\n", len - strlen(stone));
+//printf("%s\n", stone);
 }
