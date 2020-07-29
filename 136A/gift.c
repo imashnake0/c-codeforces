f88ccdd5becc967772470841f0e14762b9091fb7
Kamalesh Reddy Paluru <kamaleshpaluru07@gmail.com>
2020-07-28 20:44:56 -0400

    136A
    -question was terrible smh.

---
+++ 136A/gift.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    int n;
+    scanf("%i\n", &n);
+    int friend[n];
+    for(int i = 0; i < n; i++){
+        scanf("%i", &friend[i]);
+    }
+    int temp[n];
+    for(int j = 0; j < n; j++){
+        temp[friend[j] - 1] = j + 1;
+    }
+    for(int k = 0; k < n; k++){
+        printf("%i ", temp[k]);
+        printf("\n");
+    }
+}
